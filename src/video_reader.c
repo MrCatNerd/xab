3b4863dbad8a213387a5a98730bd87dee851409c
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-10-08 20:12:25 +0300

    cleaned up and fixed small issues in the video reader

--- src/video_reader.c
+++ src/video_reader.c
@@ -39,23 +39,15 @@ bool video_reader_open(VideoReaderState_t *state, const char *path) {
     state->av_packet = NULL;
     state->sws_scaler_ctx = NULL;
 
-    // get members
-    // todo: fix this pointer mess idk why i did that lol
-    AVFormatContext **av_format_ctx = &state->av_format_ctx;
-    AVCodecContext **av_codec_ctx = &state->av_codec_ctx;
-    int *video_stream_idx = &state->video_stream_idx;
-    AVFrame **av_frame = &state->av_frame;
-    AVPacket **av_packet = &state->av_packet;
-
     VLOG("-- Reading file: %s\n", path);
     // open file
-    *av_format_ctx = avformat_alloc_context();
-    if (av_format_ctx == NULL) {
+    state->av_format_ctx = avformat_alloc_context();
+    if (state->av_format_ctx == NULL) {
         fprintf(stderr, "Couldn't create AVFormatContext\n");
         return false;
     }
 
-    if (avformat_open_input(av_format_ctx, path, NULL, NULL) != 0) {
+    if (avformat_open_input(&state->av_format_ctx, path, NULL, NULL) != 0) {
         fprintf(stderr, "Couldn't open video file: %s\n", path);
         return false;
     }
@@ -63,15 +55,15 @@ bool video_reader_open(VideoReaderState_t *state, const char *path) {
     avformat_find_stream_info(state->av_format_ctx, NULL);
 
     VLOG("-- File format long name: %s\n",
-         (*av_format_ctx)->iformat->long_name);
+         state->av_format_ctx->iformat->long_name);
 
     AVCodecParameters *av_codec_params = NULL;
     AVCodec *av_codec = NULL;
-    *video_stream_idx = -1;
+    state->video_stream_idx = -1;
 
     // find the first valid video stream inside the file
-    for (unsigned int i = 0; i < (*av_format_ctx)->nb_streams; ++i) {
-        AVStream *stream = (*av_format_ctx)->streams[i];
+    for (unsigned int i = 0; i < state->av_format_ctx->nb_streams; ++i) {
+        AVStream *stream = state->av_format_ctx->streams[i];
 
         av_codec_params = stream->codecpar;
         av_codec = (AVCodec *)avcodec_find_decoder(av_codec_params->codec_id);
@@ -87,7 +79,7 @@ bool video_reader_open(VideoReaderState_t *state, const char *path) {
         // }
 
         if (av_codec_params->codec_type == AVMEDIA_TYPE_VIDEO) {
-            *video_stream_idx = i;
+            state->video_stream_idx = i;
             state->width = av_codec_params->width;
             state->height = av_codec_params->height;
             state->time_base = stream->time_base;
@@ -95,41 +87,49 @@ bool video_reader_open(VideoReaderState_t *state, const char *path) {
         }
     }
 
-    if (*video_stream_idx == -1) {
+    if (state->video_stream_idx == -1) {
         fprintf(stderr, "Couldn't find a valid video stream inside file: %s\n",
                 path);
         return false;
     }
 
     // setup a codec context for the decoder
-    *av_codec_ctx = avcodec_alloc_context3(av_codec);
-    if (!av_codec_ctx) {
+    state->av_codec_ctx = avcodec_alloc_context3(av_codec);
+    if (!state->av_format_ctx) {
         fprintf(stderr, "Couldn't allocate AVCodecContext\n");
         return false;
     }
 
-    if (avcodec_parameters_to_context(*av_codec_ctx, av_codec_params) < 0) {
+    if (avcodec_parameters_to_context(state->av_codec_ctx, av_codec_params) <
+        0) {
         fprintf(stderr, "Couldn't initialize AVCodecContext\n");
         return false;
     }
-    if (avcodec_open2(*av_codec_ctx, av_codec, NULL) < 0) {
+    if (avcodec_open2(state->av_codec_ctx, av_codec, NULL) < 0) {
         fprintf(stderr, "Couldn't open codec\n");
         return false;
     }
 
-    // av_codec_ctx->thread_count = 1;
-    *av_frame = av_frame_alloc();
-    if (!av_frame) {
+    if (state->av_codec_ctx->hwaccel != NULL) {
+        LOG("-- HW acceleration in use: %s\n",
+            state->av_codec_ctx->hwaccel->name);
+    } else {
+        LOG("-- no HW acceleration in use :( good luck cpu\n");
+    }
+
+    state->av_codec_ctx->thread_count = 4;
+    state->av_frame = av_frame_alloc();
+    if (!state->av_frame) {
         fprintf(stderr, "Couldn't allocate AVFrame\n");
     }
 
-    *av_packet = av_packet_alloc();
-    if (!av_packet) {
+    state->av_packet = av_packet_alloc();
+    if (!state->av_packet) {
         fprintf(stderr, "Couldn't allocate AVPacket\n");
     }
 
     state->internal_data[0] =
-        (*av_format_ctx)->streams[state->video_stream_idx]->start_time &
+        state->av_format_ctx->streams[state->video_stream_idx]->start_time &
         0xFFFFFFFFFFFFFFFF;
 
     return true;
