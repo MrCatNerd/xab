a63d5956ad20fab26ba15bae1f52fe62cc6d85f6
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2025-11-27 16:02:20 +0200

    feat(ipc): added send_all function and improved recv_exact function

--- src/ipc_utils.h
+++ src/ipc_utils.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <unistd.h>
 #include <errno.h>
 #include <stdint.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 /**
  * Reads exactly `len` bytes from `fd` into `buf`.
@@ -13,29 +14,58 @@
  *
  *  So it's basically recv_exact from rustc
  */
-ssize_t recv_exact(int fd, void *buf, size_t len, int flags) {
-    uint8_t *p = buf;
+ssize_t recv_exact(int fd, const void *buf, size_t len, int flags) {
+    uint8_t *p = (void *)buf;
     size_t remaining = len;
 
     while (remaining > 0) {
         ssize_t n = recv(fd, p, remaining, flags);
-        if (n > 0) {
-            remaining -= n;
-            p += n;
-        } else if (n == 0) {
-            // connection closed
+        if (n == 0)
             return 0;
-        } else {
-            if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                // socket temporarily has no data - return how much we got so
-                // far
+        else if (n < 0) {
+            if (errno == EINTR)
+                continue;
+            else if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                // return how much we got so far
                 return len - remaining;
-            } else {
-                // real error
+            } else
                 return -1;
-            }
         }
+        remaining -= n;
+        p += n;
     }
 
-    return len; // success
+    return (ssize_t)len; // success
+}
+
+/**
+ * Sends exactly `len` bytes from `buf` into `fd`.
+ * Returns:
+ *   0  -> EOF (socket closed)
+ *   >0 -> success, number of bytes sent (should be len)
+ *   -1 -> error (check errno)
+ *
+ *  So it's basically write_all from rustc
+ */
+ssize_t send_all(int fd, const void *buf, size_t len, int flags) {
+    uint8_t *p = (void *)buf;
+    size_t remaining = len;
+
+    while (remaining > 0) {
+        ssize_t n = send(fd, p, remaining, flags);
+        if (n == 0)
+            return 0;
+        else if (n < 0) {
+            if (errno == EINTR)
+                continue;
+            else if (errno == EAGAIN || errno == EWOULDBLOCK)
+                // return how much we got se far
+                return len - remaining;
+            else
+                return -1;
+        }
+        p += n;
+        remaining -= n;
+    }
+    return (ssize_t)len; // success
 }
